d208e99c220e729f1d5940565ac9d957eba74544
mukandalee <leonardmukanda254@gmail.com>
2023-03-06 08:11:51 +0300

    completed the lab

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,19 @@
 class Dog
+    #setter method for the breed 
+    def breed=(breed)
+        @breed = breed
+    end
+    #getter method for the breed
+    def breed
+        @breed
+    end
 
+    #setter method for the name
+    def name=(name)
+        @name = name
+    end
+    #getter method for the name
+    def name
+        @name
+    end
 end
